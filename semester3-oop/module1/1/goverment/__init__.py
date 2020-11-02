c7677edac1ef9e7a27d05dfe72b8334f017fc749
N01SEB0Mb <noisebombch@gmail.com>
2020-11-02 09:25:03 +0200

    goverment package __init__.py

--- semester3-oop/module1/1/goverment/__init__.py
+++ semester3-oop/module1/1/goverment/__init__.py
@@ -1,1 +1,28 @@
 # coding=utf-8
+"""
+Package goverment provides political object for simulating democracy system
+"""
+
+from .party import Party
+from .elector import Elector
+from .position import Position
+from .parlament import Parlament
+from .coordinates import Coordinates, LawCoordinates
+from .law import Law, ElectoralLaw, IdeologyLaw, ParlamentaryLaw
+
+__author__ = "Tiron Mykhailo"
+__version__ = "1.0.0"
+__date__ = "01.11.2020"
+
+__all__ = [
+    "Law",
+    "Party",
+    "Elector",
+    "Position",
+    "Parlament",
+    "IdeologyLaw",
+    "Coordinates",
+    "ElectoralLaw",
+    "LawCoordinates",
+    "ParlamentaryLaw",
+]
