878e05e5dce45905044548704209e61eaa5f9f84
no1sebomb <noisebombch@gmail.com>
2020-11-26 17:40:31 +0200

    lab1-2 init

---
+++ semester3-oop/lab1/figure/figure/types/axisfigure.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+from .funcsfigure import Figure
+
+
+class AxisFigure(Figure):
+    pass
---
+++ semester3-oop/lab1/figure/figure/types/func.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+
+class Function:
+    def __init__(self, expression: str) -> None:
+        pass
+
