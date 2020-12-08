4f22fa493813e381af5a77555d1579420052ecc1
no1sebomb <noisebombch@gmail.com>
2020-12-08 17:21:00 +0200

    lab3 removed extra sites

--- semester3/oop/lab3/parser/client/__init__.py
+++ semester3/oop/lab3/parser/client/__init__.py
@@ -8,12 +8,9 @@ from parser.client.AutoLider.client import AutoLider
 from parser.client.AutoTechnics.client import AutoTechnics
 from parser.client.Busmarket.client import Busmarket
 from parser.client.Direct24.client import Direct24
-from parser.client.FormParts.client import FormParts
 from parser.client.InterCars.client import InterCars
 from parser.client.Mahina.client import Mahina
 from parser.client.MasterService.client import MasterService
-from parser.client.MaxiElit.client import MaxiElit
-from parser.client.OmegaAuto.client import OmegaAuto
 from parser.client.XPertAuto.client import XPertAuto
 
 
@@ -26,11 +23,8 @@ CLIENT_CLASSES = {
     AutoTechnics,
     Busmarket,
     Direct24,
-    FormParts,
     InterCars,
     Mahina,
-    MaxiElit,
     MasterService,
-    OmegaAuto,
     XPertAuto
 }
