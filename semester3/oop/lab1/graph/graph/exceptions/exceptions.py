36c484da547d91e8cb164cc67ad8980c67d82498
no1sebomb <noisebombch@gmail.com>
2020-12-09 12:15:51 +0200

    graph exceptions fix

--- semester3/oop/lab1/graph/graph/exceptions/exceptions.py
+++ semester3/oop/lab1/graph/graph/exceptions/exceptions.py
@@ -8,21 +8,14 @@ class GraphError(Exception):
     """
     This is base graph error
     """
-    def __init__(self, *args, **kwargs):
-        super(Exception, self).__init__(*args, **kwargs)
+
+    pass
 
 
 class GraphTypeError(GraphError, TypeError):
     """
     This error occurs when there is a type mismatch in this package
     """
-    def __init__(self, *args, **kwargs):
-        super(GraphError, self).__init__(*args, **kwargs)
 
+    pass
 
-class PairError(GraphError):
-    """
-    This error occurs when an edge is attempted to initialize with wrong number of vertices
-    """
-    def __init__(self, *args, **kwargs):
-        super(GraphError, self).__init__(*args, **kwargs)
