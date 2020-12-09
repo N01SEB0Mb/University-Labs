5e90cb1249c76cefa0cc0ccb5acb896072485506
no1sebomb <noisebombch@gmail.com>
2020-12-09 15:22:35 +0200

    amgraph weight added

--- semester3/oop/lab1/graph/graph/types/amgraph.py
+++ semester3/oop/lab1/graph/graph/types/amgraph.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from warnings import warn
-from typing import Generator, Iterable, List, Set
+from typing import *
 
 import graph.exceptions
 from .vertex import Vertex
@@ -20,11 +20,11 @@ class AMGraph(GraphABC):
         Inits AMGraph
         """
 
-        self.__am: List[List[bool]] = []
+        self.__am: List[List[float]] = []
 
         super(AMGraph, self).__init__()
 
-    def add(self, vertex: Vertex, connections: Iterable[Vertex]) -> None:
+    def add(self, vertex: Vertex, connections: Iterable[Tuple[Vertex, float]]) -> None:
         # Check existence
         if vertex in self:
             raise graph.exceptions.GraphExistenceError("Vertex already exists")
@@ -36,13 +36,19 @@ class AMGraph(GraphABC):
 
         # Iterare every vertex connection
         for connection in range(vertex + 1):
-            self.__am[vertex].append(connection in connections)
+            for connect, weight in connections:
+                if connect == connection:
+                    break
+            else:
+                weight = 0.0
+
+            self.__am[vertex].append(weight)
 
             if connection != vertex:
-                self.__am[connection].append(connection in connections)
+                self.__am[connection].append(weight)
             else:
                 # If connecting to itself
-                if vertex in connections and self.LOOP_WARN:
+                if vertex in map(lambda item: item[0], connections) and self.LOOP_WARN:
                     warn(graph.exceptions.LoopWarning("Connecting vertex to itself"))
 
     def remove(self, vertex: Vertex):
@@ -61,16 +67,16 @@ class AMGraph(GraphABC):
         if start not in self or end not in self:
             raise graph.exceptions.GraphExistenceError("Given vertices does not exists")
 
-        return self.__al[start][end]
+        return bool(self.__al[start][end])
 
-    def connect(self, start: Vertex, end: Vertex) -> None:
+    def connect(self, start: Vertex, end: Vertex, weight: float = 1.0) -> None:
         # Check existence
         if start not in self or end not in self:
             raise graph.exceptions.GraphExistenceError("Given vertices does not exists")
 
         # Add connections
-        self.__al[start][end] = True
-        self.__al[end][start] = True
+        self.__al[start][end] = weight
+        self.__al[end][start] = weight
 
         # If connected to itself
         if start == end and self.LOOP_WARN:
@@ -82,8 +88,8 @@ class AMGraph(GraphABC):
             raise graph.exceptions.GraphExistenceError("Given vertices does not exists")
 
         # Remove connections
-        self.__al[start][end] = False
-        self.__al[end][start] = False
+        self.__al[start][end] = 0.0
+        self.__al[end][start] = 0.0
 
     def __iter__(self) -> Iterable[Vertex]:
         return iter([Vertex(vertex) for vertex in range(len(self.__am))])
