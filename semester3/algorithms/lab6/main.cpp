aa17b120cd1773e4b73c00230b7c7f902df67de1
no1sebomb <noisebombch@gmail.com>
2020-12-11 08:21:51 +0200

    lab6 done

--- semester3/algorithms/lab6/main.cpp
+++ semester3/algorithms/lab6/main.cpp
@@ -1,3 +1,20 @@
+#include "iostream"
+
+#include "shifted.hpp"
+
+
 int main() {
-    return 0;
+    std::string input;
+    std::string output;
+
+    // Input origin string
+    std::cout << "Type origin string: ";
+    std::cin >> input;
+
+    // Input shifted string
+    std::cout << "Type shifted string: ";
+    std::cin >> output;
+
+    // Output value
+    std::cout << isShifted(input, output) << std::endl;
 }
---
+++ semester3/algorithms/lab6/shifted.hpp
@@ -0,0 +1,38 @@
+#include "string"
+
+int isShifted(const std::string &origin,
+              const std::string &shifted) {
+    // Init values
+
+    const int n = origin.length();  // Save length of string
+
+    int s = 0;  // Sum of all chars
+    int si = 0;  // Sum of shifted chars
+
+    for (char symbol: origin) {
+        s += symbol;
+    }
+
+    // Calculate subtraction of origin and shifted
+
+    int sub = 0;
+
+    for (int i = 0; i < n; i++) {
+        sub += (i + 1) * (shifted[i] - origin[i]);
+    }
+
+    // Iterate through possible shift values
+
+    int value;
+
+    for (int d = 1; d < n; d++) {
+        si += origin[n - d]; // Add shifted char
+        value = d * s - n * si; // Calculate value for current shift
+
+        if (value == sub) {
+            return d;  // If value == sub then shift found
+        }
+    }
+
+    return -1;  // If shift not found
+}
