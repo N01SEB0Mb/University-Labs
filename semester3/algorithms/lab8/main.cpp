85715f63d470fa697918c0a2bcf50a96256ec5a1
no1sebomb <noisebombch@gmail.com>
2020-12-23 09:25:27 +0200

    lab8 init

---
+++ semester3/algorithms/lab8/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string text, pattern;
+
+    std::cout << "Type str: ";
+    std::cin >> text;
+
+    std::cout << "Type pattern you want to find: ";
+    std::cin >> pattern;
+}
