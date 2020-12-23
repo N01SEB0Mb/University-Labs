41881a8fba53d184eae415084175823b25ae7914
no1sebomb <noisebombch@gmail.com>
2020-12-23 09:48:18 +0200

    lab8 testing algorithms added

--- semester3/algorithms/lab8/main.cpp
+++ semester3/algorithms/lab8/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+
+#include "algorithms/kmp.hpp"
+#include "algorithms/naive.hpp"
+#include "algorithms/horspool.hpp"
+#include "algorithms/rabin_karp.hpp"
+
+
+std::string text;
+std::string pattern;
+
+
+template<typename FuncType>
+void calcTime(FuncType func, const std::string &funcName) {
+    // Measuring time
+    clock_t start = clock();
+    int result = func();
+    clock_t time = clock() - start;
+
+    // Output result
+    std::cout << funcName + "() -> " << result << " ";
+    // Output time
+    std::cout << "[" << time << " ms]" << std::endl;
+}
+
 
 int main() {
-    std::string text, pattern;
-
     std::cout << "Type str: ";
     std::cin >> text;
 
     std::cout << "Type pattern you want to find: ";
     std::cin >> pattern;
+
+    // Testing different algorithms
+    calcTime([](){return kmp(text, pattern);}, "KMP");
+    calcTime([](){return naive(text, pattern);}, "Naive");
+    calcTime([](){return horspool(text, pattern);}, "Horspool");
+    calcTime([](){return rabin_karp(text, pattern);}, "Rabin-Karp");
 }
