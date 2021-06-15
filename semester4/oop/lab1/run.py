156414f71da87bfbfddd0beddb174c9ef0381844
no1sebomb <noisebombch@gmail.com>
2021-06-15 17:03:44 +0300

    semester4 oop lab1 running script added

--- semester4/oop/lab1/run.py
+++ semester4/oop/lab1/run.py
@@ -1,1 +1,28 @@
 # coding=utf-8
+"""
+This script used for running UkrnetNewsBot
+"""
+
+import time
+import asyncio
+
+from ukrnet_news.bot import UkrnetNewsBot, checker
+from ukrnet_news.config import TELEGRAM
+
+
+if __name__ == "__main__":
+    # Run infinite loop
+
+    while True:
+        # Create bot instance
+        news_bot = UkrnetNewsBot(TELEGRAM["bot"]["token"])
+
+        # Run asyncronyous loop
+        async_loop = asyncio.get_event_loop()
+        async_loop.run_until_complete(refresher(news_bot))
+
+        # Close bot after 'checker' ending (some exception occurred)
+        async_loop.run_until_complete(news_bot.close())
+
+        # Sleep for configured number of seconds
+        time.sleep(10)
