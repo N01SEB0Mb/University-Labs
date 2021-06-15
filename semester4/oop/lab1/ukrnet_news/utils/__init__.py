d8b81d3ffadabb9890382b0adeed0b9a3202056b
no1sebomb <noisebombch@gmail.com>
2021-06-15 16:48:36 +0300

    semester4 oop lab1 utils asynchronous exception handler added

--- semester4/oop/lab1/ukrnet_news/utils/__init__.py
+++ semester4/oop/lab1/ukrnet_news/utils/__init__.py
@@ -1,3 +1,3 @@
 # coding=utf-8
 
-from .exc_handler import exception_handler
+from .exc_handler import exception_handler, async_exception_handler
--- semester4/oop/lab1/ukrnet_news/utils/exc_handler.py
+++ semester4/oop/lab1/ukrnet_news/utils/exc_handler.py
@@ -39,3 +39,41 @@ def exception_handler(function: Callable) -> Callable:
             return result
 
     return try_except_wrapper
+
+
+def async_exception_handler(function: Callable) -> Callable:
+    """
+    Wrapping asynchronous function in try-except expression
+
+    Args:
+        function (Callable): Function, you want to wrap
+
+    Returns:
+        Callable: Function wrapper
+    """
+
+    async def async_try_except_wrapper(*args: Any, **kwargs: Any) -> Optional[Any]:
+        """
+        Running wrapped asynchronuous function with specified arguments
+
+        Args:
+            *args (Any): 'function' *args
+            **kwargs (Any): 'function' **kwargs
+
+        Returns:
+            Optional[Any]: 'function' result
+        """
+
+        try:
+            # Try executing function
+            result: Any = await function(*args, **kwargs)
+
+        except BaseException:
+            # Some exception raised
+            return None
+
+        else:
+            # No exceptions
+            return result
+
+    return async_try_except_wrapper
