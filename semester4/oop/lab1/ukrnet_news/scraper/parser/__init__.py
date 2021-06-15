4798f8b3fb525e4572440e5d49d88557046a9549
no1sebomb <noisebombch@gmail.com>
2021-06-16 00:14:56 +0300

    semester4 oop lab1 news page parsers subpackage added

---
+++ semester4/oop/lab1/ukrnet_news/scraper/parser/__init__.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+This subpackage stores news pages parsers
+"""
+
+from typing import List, Type
+
+from .base import BaseInfoParser, MetaInfoParser
+
+
+# Define list of available parser classes
+PARSER_CLASSES: List[Type[BaseInfoParser]] = [
+    MetaInfoParser
+]
+
+
+# Define public attributes of the package
+__all__ = (
+    "MetaInfoParser",
+)
---
+++ semester4/oop/lab1/ukrnet_news/scraper/parser/base.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+
+import abc
+from typing import *
+from lxml import html
+
+
+class BaseInfoParser(abc.ABC):
+    """
+    BaseInfoParser class
+
+    Attributes:
+        url (str): Parsed url
+    """
+
+    url: str
+
+    @staticmethod
+    @abc.abstractmethod
+    def parse(html_code: str) -> Tuple[Optional[str], Optional[str], Optional[str]]:
+        """
+        Parse news HTML page and get info
+
+        Args:
+            html_code (str): HTML page you want to parse
+
+        Returns:
+            Tuple[Optional[str], Optional[str], Optional[str]): News title, description and image URL
+        """
+
+        pass
+
+
+class MetaInfoParser(BaseInfoParser):
+    """
+    MetaInfoParser class. Parses page meta-data.
+    Used if there are no parser for given url
+
+    Attributes:
+        url (str): Empty, so it could be parser for any url
+    """
+
+    url: str = ""
+
+    @staticmethod
+    def parse(html_code: str) -> Tuple[Optional[str], Optional[str], Optional[str]]:
+        """
+        Parse news HTML page and gets page metadata
+
+        Args:
+            html_code (str): HTML page you want to parse
+
+        Returns:
+            Tuple[Optional[str], Optional[str], Optional[str]): News title, description and image URL
+        """
+
+        # Parse HTML to lxml tree
+        html_tree = html.fromstring(html_code)
+
+        # Get best (longest) title or empty if there are none
+        title = max(
+            html_tree.xpath('//title/text()') +  # Page title
+            html_tree.xpath('//meta[@property="og:title" or @name="og:title"]/@content') +  # og title
+            html_tree.xpath('//meta[@property="twitter:title" or @name="twitter:title"]/@content') +  # twitter title
+            [None],  # empty title
+            key=lambda value: 0 if value is None else len(value)
+        )
+
+        # Get best (longest) description or empty if there are none
+        description = max(
+            html_tree.xpath('//div[@class="article-body"]//strong/text()') +  # Article description
+            html_tree.xpath('//meta[@property="og:description" or @name="og:description"]/@content') +  # og description
+            html_tree.xpath('//meta[@property="description" or @name="description"]/@content') +  # Page description
+            html_tree.xpath('//meta[@name="twitter:description" or @property="twitter:description"]/@content') +
+            [None],  # empty description
+            key=lambda value: 0 if value is None else len(value)
+        )
+
+        # Get first image or empty if there are none
+        image = list(
+            html_tree.xpath('//meta[@property="og:image" or @name="og:image"]/@content') +
+            html_tree.xpath('//meta[@name="twitter:image" or @property="twitter:image"]/@content') +
+            [None]
+        )[0]
+
+        # Return parsed info
+        return title, description, image
