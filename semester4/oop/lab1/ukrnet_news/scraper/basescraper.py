8fffcd74e84573bf9ef2d2e0427096d24a0bbd93
no1sebomb <noisebombch@gmail.com>
2021-06-15 18:25:18 +0300

    semester4 oop lab1 BaseNewsScraper __init__ & get_news() method done + disabled SSL warning

--- semester4/oop/lab1/ukrnet_news/scraper/basescraper.py
+++ semester4/oop/lab1/ukrnet_news/scraper/basescraper.py
@@ -2,11 +2,55 @@
 
 import abc
 import requests
+from typing import *
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+from .news import News
+
+from ukrnet_news.config import CONFIG
+
+
+# Disable InsecureRequestWarning (Unverified HTTPS)
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 class BaseNewsScraper(requests.Session, abc.ABC):
     """
     BaseNewsScraper class
+
+    Attributes:
+        timeout (int): Connection timeout
     """
 
-    pass
+    timeout: int = CONFIG["connection"]["timeout"]
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """
+        BaseNewsScraper initializing
+
+        Args:
+            *args (Any): requests.Session *args
+            **kwargs (Any): requests.Session **kwargs
+        """
+
+        super(BaseNewsScraper, self).__init__(*args, **kwargs)
+
+        # Add configured headers
+        self.headers.update(CONFIG["connection"]["headers"])
+
+        # Disable SSL verify
+        self.verify = False
+
+    @abc.abstractmethod
+    def get_news(self, category: str) -> Generator[News, None, None]:
+        """
+        Get last news from specified category
+
+        Attributes:
+            category (str): Name of category
+
+        Yields:
+            News: Last news from specified category
+        """
+
+        pass
