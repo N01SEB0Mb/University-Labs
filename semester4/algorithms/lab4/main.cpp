c6a6ebfe075218be93949f72137642e55dec9b55
no1sebomb <noisebombch@gmail.com>
2021-06-20 22:33:19 +0300

    [Sem4] #Algorithms <Lab4> Running script added

--- semester4/algorithms/lab4/main.cpp
+++ semester4/algorithms/lab4/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+#include "structure/optimal_bst.hpp"
+
 
 int main() {
-    return 0;
+    std::vector<int> values;
+    std::vector<double> probabilities;
+    std::vector<double> fict_probabilities;
+
+    // Fill vectors
+
+    for (int value = 0; value < 8; ++value) {
+        values.push_back(value + 1);
+        probabilities.push_back(0.05);
+        fict_probabilities.push_back(0.05);
+    }
+
+    // Create and output tree
+
+    auto* optimal_bst_tree = new structures::OptimalBST(values, probabilities, fict_probabilities);
+
+    print_separator("Tree:");
+
+    optimal_bst_tree->output();
 }
